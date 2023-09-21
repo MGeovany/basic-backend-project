8769d42da2965f121887bd1de86d91271800d1e8
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2023-09-21 10:58:10 -0600

    added cors

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const users = require("./routes/users");
 const books = require("./routes/books");
 const branches = require("./routes/branches");
+const cors = require("cors");
 
 const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
+
 app.listen(3000, () => {
   console.log("La aplicación está funcionando en el puerto 3000");
 });
